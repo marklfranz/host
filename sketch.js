3050b2ece3fccbe79269e25e5f2b789acf3455a0
marklfranz <franzm@ohio.edu>
2016-10-10 15:34:54 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,62 @@
 var pos = 25;
-var a = select('#moo');
+var x = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(24);
+}
 
 function draw() {
-  //background(237, 34, 93);
-  //fill(0);
-  a.position(100,pos);
-  //rect(25, pos, 50, 50);
+ 
+  background(0);
+  
+if(x < windowWidth){
+  x += 3;
+} else {
+  x = 0;
+}
+
+
+
+fill(255);
+textSize(50);
+textFont("Georgia");
+//text("Georgia", 30, 80);
+for(var i = 0; i < windowHeight; i += 70){
+  fill(i,100,100);
+  textFont("Helvetica");
+text("helvetica", x, i);
+//my(200,300,(255,100,30));
+
+if(pos > windowHeight){
+  pos = 0;
+} else if (pos < 0){
+  pos = windowHeight;
+}
+
+my(50,pos,255);
+my(100,pos,100);
+my(width/2,pos,50);
+}
+
+
+
+}
+
+function my(a,b,c){
+  noStroke();
+  fill(c,30);
+  rect(a,b,50,50);
 }
 
 function mouseWheel(event) {
- // println(event.delta);
+  println(event.delta);
   //move the square according to the vertical scroll amount
   pos += event.delta;
   //uncomment to block page scrolling
-  //return false;
+  return false;
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
 }
